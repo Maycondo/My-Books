7729bf43380ecb2529be67ab4375f9c550e69344
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-01-06 19:00:19 -0300

    Adicionandor brra de navegar no projeto.

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+
+import "./style.css"
+
+export default function Loader () {
+
+    return (
+        <div className="Loader">
+        </div>
+    )
+}
---
+++ src/components/Panelsuperior/Panelsuperior.tsx
@@ -0,0 +1,23 @@
+import "./style.css"
+
+import { IoBookSharp } from "react-icons/io5";
+
+
+
+
+export default function Panelsuperior() {
+
+
+    return (
+        <div className="Container">
+            <nav>
+                    <h1><IoBookSharp /> My Books</h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
