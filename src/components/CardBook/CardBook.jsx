0d68cf371c36753ad3f277763818dc3ef2c17950
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-01-12 20:49:06 -0300

    Um pequena alteração na card de books

--- src/components/CardBook/CardBook.jsx
+++ src/components/CardBook/CardBook.jsx
@@ -2,18 +2,19 @@ import { useEffect , useState } from "react";
 import "./style.css"
 
 import { BiSolidImageAdd } from "react-icons/bi";
+
+
 export default function CardBookAdd ({ setAddbook , onClose }) {
     if(!setAddbook) return null;
-
 
 
     return (
         <div className="Card_opacity">
             <div className="Card_add_book">
-                <button class="Button" onClick={ onClose }>
-                    <span class="X"></span>
-                    <span class="Y"></span>
-                    <div class="close">Close</div>
+                <button className="Button" onClick={ onClose }>
+                    <span className="X"></span>
+                    <span className="Y"></span>
+                    <div className="close">Close</div>
                 </button>
                    <img src="" alt={BiSolidImageAdd} />
                     <h2>Adicionar imagem do livro</h2>
