e16306ab11f0e0b02d6cedd708e2ad379186bc0f
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-03-01 21:42:59 -0300

    Implementação um Stado de avaliação dos livros adicionados.

--- src/components/CardBook/CardBook.tsx
+++ src/components/CardBook/CardBook.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import "./style.css";
-
+import { useState, useEffect } from "react";
+import { IoBook } from "react-icons/io5";
+import { IoStarOutline } from "react-icons/io5";
+import { IoStar } from "react-icons/io5";
 
 interface CardBookProps {
     isOpen: boolean;
@@ -12,16 +15,31 @@ interface CardBookProps {
         description: string;
         imageUrl: string;
         categoria: string[];
+        rating: number;
     }
 }
 
 export default function CardBook({ isOpen, onClose, book }: CardBookProps) {
+    const [rating, setRating] = useState(0);
+        
+    useEffect(() => {
+        const savedRating = localStorage.getItem(`@rating-${book.id}`);
+        if (savedRating) {
+            setRating(parseInt(savedRating, 10));
+        }
+    }, [book.id]);
+
+    const handleRating = (newRating: number) => {
+        setRating(newRating);
+        localStorage.setItem(`@rating-${book.id}`, newRating.toString());
+    }
+
     if (!isOpen) return null;
 
     return (
         <div className="card-book-open">
             <div className="card-book-header">  
-                <h1>Book Review</h1>              
+                <h1><IoBook /> Book Review</h1>              
                 <button className="Button_1" onClick={onClose}>
                     <span className="X_1"></span>
                     <span className="Y_1"></span>
@@ -31,6 +49,15 @@ export default function CardBook({ isOpen, onClose, book }: CardBookProps) {
             <div className="card-book__container">
                 <div className="card-book__text">
                     <h1>Review Book: <i>{book.title}</i></h1>
+                        <ul className="valiacao">
+                            <h3>Rating:</h3>
+                            {[...Array(5)].map((_, newRating) => 
+                                <li key={newRating} className="star-icon" onClick={() => handleRating(newRating + 1)}  
+                                style={{ cursor: "pointer", color: newRating < rating ? "#FFD700" : "#ccc"}}>
+                                    {newRating < rating ? <IoStar /> : <IoStarOutline />}
+                                </li>
+                            )}
+                        </ul>
                     <p>{book.description}</p>
                 </div>
                 <div className="card-book__image">
