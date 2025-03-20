79260e01adf112ab6e6d897c9caad5731a9cc0c9
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-03-19 21:38:54 -0300

    Fazendo algumas alteraçoes no estilo do da toolbar

--- src/components/CardBook/Toolbar.tsx
+++ src/components/CardBook/Toolbar.tsx
@@ -16,13 +16,13 @@ export default function Toolbar ({ setDescription }: ToolbarProps ) {
 
     const handleButtonClick = (format: string) => {
         setDescription((prev: string) => {
-            if (format === "list") return prev + "\n\n🞄 Item\n🞄 Item\n🞄 Item";
-            if (format === "numbered-list") return prev + "\n\n1. Item \n2. Item \n3. Item";
-            if (format === "bold") return prev + "<b>Texto em negrito</b>";
-            if (format === "italic") return prev + "<i>Texto em itálico</i>";
-            if (format === "h1") return prev + "\n\n <h1># Título H1</h1>";
-            if (format === "h2") return prev + "\n\n## <h2># Título H1</h2>";
-            if (format === "h3") return prev + "\n\n<h3># Título H1</h3>";
+                if (format === "list") return prev + "\n\n🞄 Item\n🞄 Item\n🞄 Item";
+                if (format === "numbered-list") return prev + "\n\n1. Item \n2. Item \n3. Item";
+                if (format === "bold") return prev + "<b>Texto em negrito</b>";
+                if (format === "italic") return prev + "<i>Texto em itálico</i>";
+                if (format === "h1") return prev + "\n\n <h1># Título H1</h1>";
+                if (format === "h2") return prev + "\n\n## <h2># Título H1</h2>";
+                if (format === "h3") return prev + "\n\n<h3># Título H1</h3>";
             return prev;
         })
     };
@@ -37,7 +37,6 @@ export default function Toolbar ({ setDescription }: ToolbarProps ) {
                 <button onClick={() => handleButtonClick("h1")}><BsTypeH1 /></button>
                 <button onClick={() => handleButtonClick("h2")}><BsTypeH2 /></button>
                 <button onClick={() => handleButtonClick("h3")}><BsTypeH3 /></button>
-
             </div>
         </div>
     )
