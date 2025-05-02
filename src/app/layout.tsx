a0ba56f8be33e4eb5a2a0dacf22df3fb089ffb84
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-05-02 19:36:26 -0300

    Ajustando largura da classe .My_Book para 100% e adicionando ícone de atalho no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Josefin_Sans } from "next/font/google";
+import Icone from "@/Image/Icone.png";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,7 +34,7 @@ export default function RootLayout({
     return (
       <html lang="pt-BR">
       <head>
-        
+      <link rel="shortcut icon" type="image/png" href={Icone.src}></link>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
