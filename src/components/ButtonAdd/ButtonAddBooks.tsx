8df17f53d4e3c99700c3e2edbf3b7ad6c028a901
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-05-17 16:18:22 -0300

    refatorando o componente AddBooks: simplificando a renderização do CardPassword

--- src/components/ButtonAdd/ButtonAddBooks.tsx
+++ src/components/ButtonAdd/ButtonAddBooks.tsx
@@ -35,18 +35,20 @@ export default function AddBooks() {
           />
         ) : (
           <CardPassword
-            isOpen={isAddingBook}
-            onClose={() => {
-              toggleAddBook(false);
-              setSubmitted(false);
-              setInputPassword("");
-            }}
-            submitted={submitted}
-            setSubmitted={setSubmitted}
-            inputPassword={inputPassword}
-            setInputPassword={setInputPassword}
-            passwordAdmin={PASSWORD_ADMIN}
-          />
+              isOpen={isAddingBook}
+              onClose={() => {
+                toggleAddBook(false);
+                setSubmitted(false);
+                setInputPassword("");
+
+              } }
+              submitted={submitted}
+              setSubmitted={setSubmitted}
+              inputPassword={inputPassword}
+              setInputPassword={setInputPassword}
+              passwordAdmin={PASSWORD_ADMIN} onSuccess={function (): void {
+                throw new Error("Function not implemented.");
+              } }          />
         )
       )}
 
